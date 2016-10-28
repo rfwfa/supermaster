a1706420cab4aa7c8c24d212a755c6af42b8feec
Ivan Korolevskiy <ivan.korolevskiy@emc.com>
2016-10-28 16:59:54 -0700

    Assignment 02

--- rfaRunner.py
+++ rfaRunner.py
@@ -1,12 +1,27 @@
 '''
 Created on Oct 19, 2016
 
-@author: sashaalexander
-'''
-from rfaUtils import getLog,qaprint
 
+@author: sashaalexander
+@author: team X
+'''
+from rfaUtils import getLog,qaPrint
+
+import sys
+
+# get the log file handle
 log = getLog()
 
-message = "It is working, right?"
-qaprint(log,message)
+# exit if log creation failed
+if log == -1:
+    sys.exit("Unable to create log file")
 
+message = "It is working, right?"
+
+# call qaPrint to print a message with timestamp and write it to the log file
+qaPrint(log, message)
+qaPrint(log, "Me like what me see")
+
+# close the log file if it open
+if not log.closed:
+    log.close()
